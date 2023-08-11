e5f2f2966a6b27b8bf67b6a87d54cff68c40809c
Vasanth Ravi <100667947+rkvasanth@users.noreply.github.com>
2023-08-11 13:21:24 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,31 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+console.log(header)
+window.onscroll = function() {
+    const top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+// collapse navbar after click on small devices
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
+})
+
+let number = document.getElementById("number");
+let counter = 0;
+setInterval(() => {
+	if (counter == 65) {
+		clearIntreval();
+	}
+	else{
+counter += 1;
+number.innerHTML = counter +  "%";
+    }
+}, 30);
