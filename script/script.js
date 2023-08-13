9a5b9b493d10f7e19c1a7225c57492e69f84a6e2
Vasanth Ravi <100667947+rkvasanth@users.noreply.github.com>
2023-08-14 00:20:02 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -21,11 +21,71 @@ navLinks.forEach((l) => {
 let number = document.getElementById("number");
 let counter = 0;
 setInterval(() => {
-	if (counter == 65) {
+	if (counter == 75) {
 		clearIntreval();
 	}
 	else{
 counter += 1;
 number.innerHTML = counter +  "%";
+    }
+}, 30);
+
+let number1 = document.getElementById("numberOne");
+let counter1 = 0;
+setInterval(() => {
+	if (counter1 == 60) {
+		clearIntreval();
+	}
+	else{
+counter1 += 1;
+number1.innerHTML = counter1 +  "%";
+    }
+}, 30);
+
+let number2 = document.getElementById("numberTwo");
+let counter2 = 0;
+setInterval(() => {
+	if (counter2 == 55) {
+		clearIntreval();
+	}
+	else{
+counter2 += 1;
+number2.innerHTML = counter2 +  "%";
+    }
+}, 30);
+
+let number3 = document.getElementById("numberThree");
+let counter3 = 0;
+setInterval(() => {
+	if (counter3 == 80) {
+		clearIntreval();
+	}
+	else{
+counter3 += 1;
+number3.innerHTML = counter3 +  "%";
+    }
+}, 30);
+
+let number4 = document.getElementById("numberFour");
+let counter4 = 0;
+setInterval(() => {
+	if (counter4 == 90) {
+		clearIntreval();
+	}
+	else{
+counter4 += 1;
+number4.innerHTML = counter4 +  "%";
+    }
+}, 30);
+
+let number5 = document.getElementById("numberFive");
+let counter5 = 0;
+setInterval(() => {
+	if (counter5 == 60) {
+		clearIntreval();
+	}
+	else{
+counter5 += 1;
+number5.innerHTML = counter +  "%";
     }
 }, 30);
